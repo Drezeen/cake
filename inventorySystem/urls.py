9341053f4d7f54449908ee0c1783086b4b0d737e
Drezeen <africanblackgodzilla@gmail.com>
2024-08-16 18:25:12 +0300

    MAD

--- inventorySystem/urls.py
+++ inventorySystem/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,7 @@ urlpatterns = [
     path("",auth_views.LogoutView.as_view(template_name = "inventory_system/logout.html"), name="logout")
 ]
 
-urlpatterns =+ staticfiles_urlpatterns()
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
